47c0d3865ba49596f77a6c644915d941742558dc
CheSangJung <shtingstar@hanmail.net>
2021-09-10 10:50:13 +0900

    리액트 컴포넌트 타입스크립트로 작성하기

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Greeting from "./Greeting";
 
 const App: React.FC = () => {
+  const handleClick = (name:string) => {
+    console.log(`${name} says hello`)
+  }
   return (
-    <Greeting name={"Sydney"} />
+    <Greeting name={"Sydney"} onClickGreeting={handleClick} />
   )
 }
 
--- src/Components/Greeting.tsx
+++ src/Components/Greeting.tsx
@@ -4,6 +4,8 @@ type GreetingsProps = {
     name: string;
     mark: string;
     optional?: string;
+    onClickGreeting: (name : string) => void; 
+    // void 아무것도 리턴하지 않는 함수라는 뜻
 };
 
 // const Greeting: React.FC<GreetingsProps> = ({ name, mark }) => (
@@ -12,13 +14,18 @@ type GreetingsProps = {
 // const Greeting = ({name, mark} : GreetingsProps) => (
 //     <div>Hello, {name} {mark}</div>
 // )
-const Greeting = ({name, mark, optional} : GreetingsProps) => {
+const Greeting = ({name, mark, optional, onClickGreeting} : GreetingsProps) => {
+    const handleClick = () => onClickGreeting(name);
     return (
-        <div>
-            Hello, {name} {mark}
-            {optional && <p>{optional}</p>}
-        </div>
-        
+        <>
+            <div>
+                Hello, {name} {mark}
+                {optional && <p>{optional}</p>}
+            </div>
+            <div>
+                <button onClick = {handleClick}>click me</button>
+            </div>
+        </>
     )
 }
 
